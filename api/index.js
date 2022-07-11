55d27e0063bb45cf032ce022e8ca5d70bc5c0fe0
“brenda-stefany-americo-de-almeida” <brendamerico1@gmail.com>
2022-07-11 10:12:17 -0300

    Removendo useMongoClient

--- api/index.js
+++ api/index.js
@@ -4,8 +4,7 @@ var mongoose = require('mongoose');
 
 //Conexão com Banco de Dados
 mongoose.Promisse = global.Promisse;
-mongoose.connect('mongodb://localhost:27017/projeto-fullstack-web',
-{useMongoClient: true})
+mongoose.connect('mongodb://localhost:27017/projeto-fullstack-web')//,{useMongoClient: true}
     .then(() => {
         console.log("Conexão realizada com sucesso!")
     })
